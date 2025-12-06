2ef427c7e7deed623e6c390ebd6eb6b2d6b1923a
Naiya <154001853+ninikim89@users.noreply.github.com>
2025-12-06 16:36:30 +0530

    virtual_mouse.py

--- virtual_mouse.py
+++ virtual_mouse.py
@@ -3,11 +3,10 @@ import mediapipe as mp
 import pyautogui
 import math
 
-# 1. SETUP
+
 cap = cv2.VideoCapture(0)
 screen_width, screen_height = pyautogui.size()
 
-# 2. AI HAND TRACKER
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1,
                        min_detection_confidence=0.5, min_tracking_confidence=0.5)
@@ -61,4 +60,5 @@ while True:
         break
 
 cap.release()
+
 cv2.destroyAllWindows()
